ab7254d5c15e31e451b8a4f38d91bd031985406e
Brian Schlining <bschlining@gmail.com>
2018-03-01 14:07:17 +1100

    Updated main class name in launch scripts

--- src/main/assembly/files/bin/query.bat
+++ src/main/assembly/files/bin/query.bat
@@ -5,4 +5,4 @@ SET VARS_HOME=%~dp0..
 SET VARS_CLASSPATH="%VARS_HOME%\conf";"%VARS_HOME%\lib\*"
 
 echo [VARS] Starting VARS Query Application
-java -cp %VARS_CLASSPATH% -Xms128m -Xmx256m -Duser.timezone=UTC vars.query.ui.App
+java -cp %VARS_CLASSPATH% -Xms128m -Xmx256m -Duser.timezone=UTC org.mbari.m3.vars.query.App
