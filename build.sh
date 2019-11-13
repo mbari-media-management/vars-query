31ea1eb40c116c91c1c02e29c35b158728e949f5
Brian Schlining <bschlining@gmail.com>
2019-11-12 17:08:33 -0800

    M3-59: Changes to deploy as DMG

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@
 
 export JPACKAGE_HOME="$HOME/Applications/jdk-14.jdk/Contents/Home"
 source "$HOME/workspace/m3-deployspace/vars-query/env-config.sh"
-gradle clean jpackage
+gradle jpackage --info
+
+
+#/Users/brian/Applications/jdk-14.jdk/Contents/Home/bin/jpackage --type dmg --dest /Users/brian/workspace/vars-query/build/jpackage --name "VARS Query" --module-path /Users/brian/workspace/vars-query/build/jlinkbase/jlinkjars --module org.mbari.vars.query/org.mbari.m3.vars.query.App --app-version 0.1.1 --runtime-image /Users/brian/workspace/vars-query/build/image --java-options -Xms1g --java-options -DANNOSAURUS_JDBC_DRIVER=com.microsoft.sqlserver.jdbc.SQLServerDriver --java-options -DANNOSAURUS_JDBC_PASSWORD=guest --java-options -DANNOSAURUS_JDBC_URL="jdbc:sqlserver://perseus.shore.mbari.org:1433;databaseName=M3_ANNOTATIONS" --java-options -DANNOSAURUS_JDBC_USER=everyone --java-options -DCONCEPT_SERVICE_TIMEOUT=5seconds --java-options -DCONCEPT_SERVICE_URL="http:/m3.shore.mbari.org/kb/v1" --java-options -DSHARKTOPODA_PORT=8800 --java-options -DVARS_QUERY_ANNOTATION_START_DATE=1982-01-01T00:00:00Z --java-options -DVARS_QUERY_FRAME_TITLE="VARS Query"
