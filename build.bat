9f81086522175c66385dcc6d59aa7559b63e2fa6
Brian Schlining <bschlining@gmail.com>
2020-01-13 13:29:51 -0800

    modified build for windows

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
 :: Build script used internally at MBARI
-SET JPACKAGE_HOME="%USERPROVILE%/Desktop/jdk-14
-CALL "%USERPROVILE%/workspace/m3-deployspace/vars-query/env-config.bat"
+SET JPACKAGE_HOME="C:\Users\brian\Desktop\jdk-14"
+CALL "Z:/workspace/m3-deployspace/vars-query/env-config.bat"
 gradle jpackage --info
