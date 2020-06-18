d483162afacc56cf74496a2237882893a8186f47
Brian Schlining <bschlining@gmail.com>
2020-06-18 16:52:07 -0700

    Working on a windows build

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 :: Build script used internally at MBARI
-SET JPACKAGE_HOME="C:\Users\brian\Desktop\jdk-14"
-CALL "Z:/workspace/m3-deployspace/vars-query/env-config.bat"
-gradle jpackage --info
+REM SET JPACKAGE_HOME="C:\Users\brian\Desktop\jdk-14"
+REM CALL "Z:/workspace/m3-deployspace/vars-query/env-config.bat"
+CALL "%1"
+gradlew jpackage --info
